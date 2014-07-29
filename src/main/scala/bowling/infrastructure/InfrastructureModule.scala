b1cc3f53174ec73e961149e865eaae1c09819ae2
WadeW <wwaldron@point2.com>
2014-07-29 14:38:02 -0600

    Create an instance of a Match Repo.

--- src/main/scala/bowling/infrastructure/InfrastructureModule.scala
+++ src/main/scala/bowling/infrastructure/InfrastructureModule.scala
@@ -1,7 +1,9 @@
 package bowling.infrastructure
 
 import bowling.core.Module
+import scorecards.DefaultMatchRepository
+import bowling.domain.MatchRepository
 
 trait InfrastructureModule extends Module {
-
+  lazy val matchRepository:MatchRepository = new DefaultMatchRepository
 }
